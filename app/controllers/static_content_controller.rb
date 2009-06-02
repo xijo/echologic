da6efe0623dcdc1430d365ff52734616613c0927
Johannes Opper <johannes.opper@echologic.org>
2009-06-02 15:12:48 +0000

    added the StaticContentController with a first view and a specific route

---
+++ app/controllers/static_content_controller.rb
@@ -0,0 +1,53 @@
+class StaticContentController < ApplicationController
+  
+  def echologic
+    respond_to do |format|
+      format.html
+    end
+  end
+  
+  def echo
+    
+  end
+  
+  def echo_discuss
+    
+  end
+
+  def echo_connect
+    
+  end
+
+  def echo_act
+    
+  end
+
+  def echocracy
+    
+  end
+
+  def echocracy_cititens
+    
+  end
+  
+  def echocracy_experts
+    
+  end
+  
+  def echocracy_orgas
+    
+  end
+  
+  def echonomy_business_model
+    
+  end
+
+  def echonomy_open_source
+    
+  end
+  
+  def echonomy_public_property
+    
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
+  
+  map.start '/echologic', :controller => 'staticContent', :action => 'echologic'  
+
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
