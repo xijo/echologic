7bf32ffed47d69dc70a33908e90b1e0118f1a621
Laszlo Papp <laszlo.papp@echologic.org>
2009-06-03 16:38:10 +0000

    YML and views adjusted to better support conventions.

--- config/routes.rb
+++ config/routes.rb
@@ -56,6 +56,7 @@ ActionController::Routing::Routes.draw do |map|
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
   # map.root :controller => "welcome"
+  map.root :controller => 'static_content'
 
   # See how all your routes lay out with "rake routes"
 
