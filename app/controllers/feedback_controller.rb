96d58ebf601cf23ce510c9083907fe535207931a
Johannes Opper <johannes.opper@echologic.org>
2009-06-30 10:15:50 +0000

    mail functionality, configuration and basic feedback implementation

---
+++ app/controllers/feedback_controller.rb
@@ -0,0 +1,25 @@
+class FeedbackController < ApplicationController
+
+  uses_tiny_mce :options => {
+                            :theme => 'advanced',
+                            :language => 'de',
+                            :theme_advanced_resizing => true,
+                            :theme_advanced_resize_horizontal => false
+                          }
+  
+  # POST /feedback
+  def create
+    FeedbackMailer.deliver_feedback(params)
+    respond_to do |format|
+      format.html { render :template => 'feedback/create', :layout => 'static' }
+    end
+  end
+  
+  # GET /feedback/new
+  def new
+    respond_to do |format|
+      format.html { render :template => 'feedback/new', :layout => 'static' }
+    end
+  end  
+  
+end
