1c229e4fd6d81435e731cc7fd9b1d20b3f211476
Johannes Opper <johannes.opper@echologic.org>
2009-08-14 00:23:28 +0000

    feedback i18n changes, twitter date localized

--- app/models/invited_person.rb
+++ app/models/invited_person.rb
@@ -1,5 +1,11 @@
 class InvitedPerson < ActiveRecord::Base
   
+  validates_presence_of :name, :email
+  validates_uniqueness_of :email
+  validates_format_of :email,
+    :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i,
+    :on => :create
+  
   belongs_to :interested_person
   
 end
