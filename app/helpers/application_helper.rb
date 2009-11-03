252e8af07f4b4fff81cda7e98c0bccca82ac0aa1
Johannes Opper <johannes.opper@echologic.org>
2009-11-03 21:30:38 +0100

    acl9helpers identification changed to match gem namespace.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
-  include Acl9Helpers
+  include Acl9::Helpers
 
   # If user is an admin the admin options should be shown.
   access_control :show_admin_options? do
