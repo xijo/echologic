b2f10cc1ddf14f42ead8a4e079267ac050ebb0eb
Johannes Opper <johannes.opper@echologic.org>
2009-11-03 22:51:17 +0100

    diverse bugfixing.

--- app/models/report.rb
+++ app/models/report.rb
@@ -2,11 +2,11 @@ class Report < ActiveRecord::Base
 
   belongs_to :suspect,  :class_name => 'User', :foreign_key => 'suspect_id'
   belongs_to :reporter, :class_name => 'User', :foreign_key => 'reporter_id'
-  
-  validates_presence_of :reason
-  
+
+  validates_presence_of :reason, :suspect_id, :reporter_id
+
   # TODO helpful but not working yet.
   self.named_scope :active, :conditions => { :done => false }
   self.named_scope :done,   :conditions => { :done => true }
-  
+
 end
