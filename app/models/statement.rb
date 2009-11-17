ae118f7f711b5e77282c4c53828f2a820fce7ac2
niklas <niklas@brueckenschlaeger.de>
2009-11-17 21:00:11 +0100

    named scopes for subtypes

--- app/models/statement.rb
+++ app/models/statement.rb
@@ -6,7 +6,19 @@ class Statement < ActiveRecord::Base
   belongs_to :root_statement, :foreign_key => "root_id", :class_name => "Statement"
   acts_as_tree :scope => :root_statement
   
-  belongs_to :work_package
+  named_scope :proposals, lambda {
+    { :conditions => { :type => 'Proposal' } } }
+  named_scope :improvement_proposals, lambda {
+    { :conditions => { :type => 'ImmprovementProposals' } } }
+  named_scope :arguments, lambda { 
+    { :conditions => ['type = ? OR type = ?', 'ProArgument', 'ContraArgument'] } }
+  named_scope :pro_arguments, lambda { 
+    { :conditions => { :type => 'ProArgument' } } }
+  named_scope :contra_arguments, lambda { 
+    { :conditions => { :type => 'ContraArgument' } } }
+  
+  
+  belongs_to :work_packages
   
   validates_associated :creator, :document
   
