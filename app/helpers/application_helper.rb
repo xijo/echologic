fb4ae1d2126dfb4b6bf5bd97f4e32915dccb300e
Laszlo Papp <laszlo.papp@echologic.org>
2009-11-18 17:38:06 +0100

    Enhanched Roadmap function for all 3 functional areas (DCA).

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,7 +11,7 @@ module ApplicationHelper
   def insert_progressbar(percent)
     tooltip = I18n.t('application.roadmap.progress_tooltip', :progress => percent)
     val =  "<span id='roadmap_progressbar' class='ttLink' title='#{tooltip}'></span>"
-    val += "<script type='text/javascript'>$('#roadmap_progressbar').progressbar({value: #{percent}});</script>"
+    val += "<script type='text/javascript'>$('#roadmap_progressbar').progressbar({value: #{percent != 0 ? percent : 1}});</script>"
     val
   end
 
