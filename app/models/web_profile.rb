e17d2c54f2d0a5c002b3dd25d1fe621992d64d6d
jrw <jrw@lieblingscomputer2.brueckenschlaeger>
2010-02-23 12:24:58 +0100

    test coverage

--- app/models/web_profile.rb
+++ app/models/web_profile.rb
@@ -26,6 +26,6 @@ class WebProfile < ActiveRecord::Base
   validates_inclusion_of :sort, :in => WebProfile.sorts
 
   # Validate if location has valid format
-  validates_format_of :location, :with => /^((www\.|http:\/\/)([a-z0-9]*\.)+([a-z]{2,3}){1}(\/[a-z0-9]+)*(\.[a-z0-9]{1,4})?)|(([a-z0-9)+[a-z0-9\.\_-]*)@[a-z0-9]{1,}[a-z0-9-\.]*\.[a-z]{2,4})$/i
+  validates_format_of :location, :with => /^((www\.|http:\/\/)([a-z0-9]*\.)+([a-z]{2,3}){1}(\/[a-z0-9]+)*(\.[a-z0-9]{1,4})?)|(([a-z0-9]+[a-z0-9\.\_\-]*)@[a-z0-9]{1,}[a-z0-9\-\.]*\.[a-z]{2,4})$/i
 
 end
